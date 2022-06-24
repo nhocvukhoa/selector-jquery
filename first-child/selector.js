871dab1d47d439cdd3dbe03b89b8c1c8298a51be
nhocvukhoa <71363631+nhocvukhoa@users.noreply.github.com>
2022-06-24 16:27:25 +0700

    :first-child selector

--- first-child/selector.js
+++ first-child/selector.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
   //first-child: Chọn các phần tử con ở vị trí đầu tiên
   //first: Chọn phần tử ở vị trí đầu tiên
-  $("ul li:first-child").css("background", "red");
+  $("ul li:first-child").css("background", "gray");
 });
