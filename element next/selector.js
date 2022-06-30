fe16bff08ed95daa39b40e5425c5485977280088
nhocvukhoa <71363631+nhocvukhoa@users.noreply.github.com>
2022-06-30 13:12:31 +0700

    element next selector

---
+++ element next/selector.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  //element + next: Chọn phần tử tiếp theo  đầu tiên của phần tử được chỉ định 
+  $('.content1 + span').css('color', 'red');
+  $('.content2 + p').css('color', 'green');
+});
